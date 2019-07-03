8323917b00e32f4aa791baac4d7f43ee4be68335
u4bi <rootcode10@gmail.com>
2019-07-03 12:21:38 +0900

    feat(redux): Added connect redux to index page.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-import { Fragment } from 'react'
+import { Component, Fragment } from 'react'
 import Helmet from 'react-helmet'
+
+import { connect } from 'react-redux'
+import { StateObservable } from 'redux-observable'
+import { of, Subject } from 'rxjs'
+
+import epics from '../store/epcis'
+import { fetchGithub } from '../store/actions'
 
 import styles from '../static/styles/index.scss'
 
@@ -8,29 +15,70 @@ import svg from '../static/images/svg.svg'
 
 import Layout from '../components/layout'
 
-const Index = () => (
-    <Fragment>
+class Index extends Component {
 
-        <Helmet
-            title='index page, nextjs-stater'
-            meta={[
-                { 
-                    property: 'og:title',
-                    content: 'index page, nextjs-stater'
-                }
-            ]}
-        />
+    static async getInitialProps (pageProps) {
 
-        <Layout childrenStyles={ styles.index }>
+        const { store } = pageProps
 
-            <img src={ png } alt='' />
-            <img src={ svg } alt='' />
+        const store$ = new StateObservable(new Subject(), store.getState())
 
-            <p>nextjs-stater</p>
+        const action = await epics(
+                                of(fetchGithub()),
+                                store$
+                            )
+                            .toPromise()
 
-        </Layout>
+        store.dispatch(action)
 
-    </Fragment>
-)
+        return {
+            mock : 'abc'
+        }
 
-export default Index
+    }
+
+    render() {
+
+        const { data } = this.props
+
+        return (
+            <Fragment>
+
+                <Helmet
+                    title='index page, nextjs-stater'
+                    meta={[
+                        { 
+                            property: 'og:title',
+                            content: 'index page, nextjs-stater'
+                        }
+                    ]}
+                />
+
+                <Layout childrenStyles={ styles.index }>
+
+                    <img src={ png } alt='' />
+                    <img src={ svg } alt='' />
+
+                    <p>nextjs-stater</p>
+
+                </Layout>
+
+            </Fragment>
+        )
+
+    }
+
+}
+
+const mapState = ({ app : { data } }) => ({
+    data
+})
+
+const mapDispatch = {
+    fetchGithub
+}
+
+export default connect(
+    mapState,
+    mapDispatch
+)(Index)
