2f5712c37aab0e163ec74a83a8c36b589a276226
u4bi <rootcode10@gmail.com>
2019-07-04 19:58:13 +0900

    feat(webpack): Updated .gitignore, Added webpack.server.js

---
+++ webpack.server.js
@@ -0,0 +1,9 @@
+const slsw = require('serverless-webpack')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+    mode: 'production',
+    entry: slsw.lib.entries,
+    target: 'node',
+    externals: [ nodeExternals() ]
+}
