b7711aa042d92d2c902ff2ccedee1d9cc0ea8560
u4bi <rootcode10@gmail.com>
2019-10-30 18:47:56 +0900

    feat: Add file system based dynamic routing. - https://nextjs.org/blog/next-9#dynamic-route-segments

---
+++ pages/profile/[username].js
@@ -0,0 +1,102 @@
+import { Component, Fragment } from 'react'
+import NextSeo from 'next-seo'
+
+import { connect } from 'react-redux'
+import { of } from 'rxjs'
+
+import epics from '../../store/epics'
+import { fetchGithub } from '../../store/modules/github'
+
+import Layout from '../../components/Layout'
+
+class Profile extends Component {
+
+    static async getInitialProps (pageProps) {
+
+        const { isServer, query: { username } } = pageProps
+
+        if (isServer) {
+
+            const { store } = pageProps
+
+            const action = await epics(
+                                    of(fetchGithub(username))
+                                )
+                                .toPromise()
+
+            store.dispatch(action)
+
+        }
+
+        return { 
+            isServer,
+            username
+        }
+
+    }
+
+    constructor(props) {
+
+        super(props)
+
+        const { isServer, data } = props
+
+        if (!isServer && Object.keys(data).length === 0) {
+
+            const { fetchGithub, username } = props
+
+            fetchGithub(username)
+
+        }
+
+    }
+
+    render() {
+
+        const { pending, data } = this.props
+
+        const { avatar_url, name, bio } = data
+
+        const SEO = { 
+            title: `Profile - ${ name }`
+        }
+
+        return (
+            <Fragment>
+
+                <NextSeo config={ SEO } />
+
+                <Layout >
+
+                    { pending ? <h1> Loading... </h1> : <div>
+
+                        <img src={ avatar_url } alt=''/>
+
+                        <p>{ name }</p>
+
+                        <p>{ bio }</p>
+
+                    </div> }
+
+                </Layout>
+
+            </Fragment>
+        )
+
+    }
+
+}
+
+const mapState = ({ github: { pending, data } }) => ({
+    pending,
+    data
+})
+
+const mapDispatch = {
+    fetchGithub
+}
+
+export default connect(
+    mapState,
+    mapDispatch
+)(Profile)
