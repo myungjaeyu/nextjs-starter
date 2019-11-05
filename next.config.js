3846bc1475c53baf5b45088ea1f2b1ef6016cdbd
u4bi <rootcode10@gmail.com>
2019-11-05 11:34:00 +0900

    chore: Deleted next-optimized-images.

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@ const withPlugins = require('next-compose-plugins')
 
 const withCss = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
-const withOptimizedImages = require('next-optimized-images')
 const withPWA = require('next-pwa')
 
 module.exports = withPlugins([
     [ withCss, { }],
     [ withSass, { cssModules: true }],
-    [ withOptimizedImages, { }],
     [ withPWA, { }]
 ], {
 
