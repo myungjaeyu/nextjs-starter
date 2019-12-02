f5d0f60432692dd2606447c1535a44b06603dbf7
u4bi <rootcode10@gmail.com>
2019-12-02 19:36:26 +0900

    chore: Deleted @zeit/next-css.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const withPlugins = require('next-compose-plugins')
 
-const withCss = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
 const withPWA = require('next-pwa')
 
 module.exports = withPlugins([
-    [ withCss, { }],
     [ withSass, { cssModules: true }],
     [ withPWA, { }]
 ], {
